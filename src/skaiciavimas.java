797b4128a2453639eecc305b26837eee5348cb55
Marius <mariusnaudzius7@gmail.com>
2017-03-03 20:48:12 +0200

    pagrindine helo clas

--- src/skaiciavimas.java
+++ src/skaiciavimas.java
@@ -1,5 +1,13 @@
-/**
- * Created by Marius on 2017-03-03.
- */
+import java.util.Scanner;
+
 public class skaiciavimas {
-}
+    public static void main(String[] args) {
+        System.out.println("Iveskite zodi");
+        Scanner sc = new Scanner(System.in);
+        String zodis = sc.next();
+        System.out.println(zodis.length());
+
+        }
+    }
+
+
