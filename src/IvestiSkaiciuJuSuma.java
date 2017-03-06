b758da77e2ca96180d1c66bc76fb0c829fc43f18
Marius <mariusnaudzius7@gmail.com>
2017-03-06 21:11:42 +0200

    :)

---
+++ src/IvestiSkaiciuJuSuma.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class IvestiSkaiciuJuSuma {
+    public static void main(String[] args) {
+        int x = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Įveskite Skaiciu");
+        int suma = 0;
+        x= sc.nextInt();
+        while (x!=0);
+        suma += x;
+        x= sc.nextInt();
+    }
+
+    }
