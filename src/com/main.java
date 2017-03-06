7d44e5c969c6c3927739088012c21bd5099cbc0b
Marius <mariusnaudzius7@gmail.com>
2017-03-06 20:13:05 +0200

    :)

--- src/com/main.java
+++ src/com/main.java
@@ -6,9 +6,18 @@ import com.uzduotis3.Uzduotis3;
 import com.uzduotis4.Uzduotis4;
 import com.uzduotis5.Uzduotis5;
 
+import java.util.Scanner;
+
 public class main {
 
     public static void main(String[] args){
+        System.out.print("Iveskite skaiciu");
+        Scanner sc = new Scanner(System.in);
+        int baba = sc.nextInt();
+        if (baba == 1){
+            Uzduotis1 uzduotis1 = new Uzduotis1();
+        }
+
          Uzduotis1 uzduotis1 = new Uzduotis1();
  Uzduotis2 uzduotis2 = new Uzduotis2();
  Uzduotis3 uzduotis3 = new Uzduotis3();
---
+++ src/lyginisNelyginis.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+    public class lyginisNelyginis {
+        public static void main(String[] args) {
+            System.out.println("Įveskite skaičių");
+            Scanner in = new Scanner(System.in);
+            Integer x = in.nextInt();
+            if ((x % 2) == 0)
+            {
+                System.out.println("lyginis");
+            }
+            else
+            {
+                System.out.println("nelyginis");
+            }
+        }
+    }
+
