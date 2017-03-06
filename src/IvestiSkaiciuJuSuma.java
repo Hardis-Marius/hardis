d3e8439d42207aabce999843bb6ad8cd0826e42e
Marius <mariusnaudzius7@gmail.com>
2017-03-06 21:15:37 +0200

    :)

--- src/IvestiSkaiciuJuSuma.java
+++ src/IvestiSkaiciuJuSuma.java
@@ -2,14 +2,19 @@ import java.util.Scanner;
 
 public class IvestiSkaiciuJuSuma {
     public static void main(String[] args) {
-        int x = 0;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Įveskite Skaiciu");
-        int suma = 0;
-        x= sc.nextInt();
-        while (x!=0);
-        suma += x;
-        x= sc.nextInt();
-    }
+        System.out.println("Vesk pirmą skaičių");
+        Scanner in = new Scanner(System.in);
+        Integer x = in.nextInt();
+        int y = x;
+        System.out.println("Vesk antrą skaičių");
+        while (x !=0) {
+            System.out.println("Vesk dar vieną skaičių");
+            x = in.nextInt();
+            y = x + y;
+            System.out.println("Dabartinė suma " + y);
 
+
+        }
+        System.out.println("Viskas, suma" + y);
     }
+}
