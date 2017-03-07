43c357cc9959655fb93198e3171a2e7fb93122ff
Marius <mariusnaudzius7@gmail.com>
2017-03-07 21:05:35 +0200

    :)

--- src/com/Uzduotis/Uzduotis1_6.java
+++ src/com/Uzduotis/Uzduotis1_6.java
@@ -1,16 +1,30 @@
 package com.Uzduotis;
-
-
 import java.util.Scanner;
 
-public class Uzduotis1_6 {
-    public Uzduotis1_6(){}
 
-    public static void main(String[] args) {
-        {
-            System.out.println("Įveskite žodzius");
-            Scanner in = new Scanner(System.in);
-            String x = in.next();
+public class Uzduotis1_6 {
+
+    public Uzduotis1_6() {
+    }
+
+    public void nuskaitymas2() {
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("ivesk kiek nori zodziu");
+
+        int n = s.nextInt();
+
+        String arr[] = new String[n];
+
+        System.out.println("vesk " + n + " zodzius");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = s.next();
+
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
         }
     }
 }
